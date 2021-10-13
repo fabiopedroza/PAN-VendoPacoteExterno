fd447fcf6bc2645b0e42321926927f74131787ce
fpedrozam <52209755+fpedrozam@users.noreply.github.com>
2021-10-13 15:32:34 -0300

    Vendo Pacote Externo

---
+++ src/interno/Produto.java
@@ -0,0 +1,15 @@
+package interno;
+
+public class Produto {
+	
+	protected String nomeProd;
+
+	public String getNomeProd() {
+		return nomeProd;
+	}
+
+	public void setNomeProd(String nomeProd) {
+		this.nomeProd = nomeProd;
+	}
+
+}
